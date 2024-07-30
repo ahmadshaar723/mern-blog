a5b52e9ded6f58520c1ba38fbecbed077adf1acb
ahmadshaar723 <ahmadshaar723@gmail.com>
2024-07-31 00:38:42 +0300

    make the dashboard private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
             arrowIcon={false}
             inline
             label={
-              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+              <Avatar alt="user" img={currentUser.profilePicture} rounded/>
             }
           >
             <Dropdown.Header>
