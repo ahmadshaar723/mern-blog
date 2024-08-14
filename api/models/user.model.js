f18c3893c0c75ae4323683d28626c74d82cf68bb
ahmadshaar723 <ahmadshaar723@gmail.com>
2024-08-14 21:56:02 +0300

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,26 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type:String,
-        default:'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png',
+    profilePicture: {
+      type: String,
+      default:
+        "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png",
     },
-},{timestamps:true}
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
