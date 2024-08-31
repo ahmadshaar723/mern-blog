0b67a03f566e48efa841556eed22fb43f00bffe3
ahmadshaar723 <ahmadshaar723@gmail.com>
2024-08-31 16:13:27 +0300

    add call to action to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,9 +1,10 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 const PostPage = () => {
-  const  {postSlug}  = useParams();
+  const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
@@ -13,7 +14,7 @@ const PostPage = () => {
       try {
         setLoading(true);
         const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
-        const data =await res.json();
+        const data = await res.json();
         if (!res.ok) {
           setError(true);
           setLoading(false);
@@ -29,28 +30,48 @@ const PostPage = () => {
         setError(true);
       }
     };
-    fetchPost()
+    fetchPost();
   }, [postSlug]);
-  console.log(post)
-  if (loading) return (
-<div className="flex justify-center items-center min-h-screen">
-    <Spinner size='xl'/>
-</div>  
-)
+  console.log(post);
+  if (loading)
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Spinner size="xl" />
+      </div>
+    );
   return (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen ">
-        <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">{post && post.title}</h1>
-        <Link className="self-center mt-5" to={`/search?category=${post && post.category}`}>
-        <Button color='gray' pill size='xs'>{post && post.category}</Button>
-        </Link>
-        <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-cover" />
-        <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-6xl text-xs">
-            <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-            <span className="italic">{post && (post.content.length /1000).toFixed(0)} mins read</span>
-        </div>
-        <div dangerouslySetInnerHTML={{__html:post && post.content}} className="p-3 max-w-2xl mx-auto w-full post-content"></div>
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {post && post.title}
+      </h1>
+      <Link
+        className="self-center mt-5"
+        to={`/search?category=${post && post.category}`}
+      >
+        <Button color="gray" pill size="xs">
+          {post && post.category}
+        </Button>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+      />
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-6xl text-xs">
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className="italic">
+          {post && (post.content.length / 1000).toFixed(0)} mins read
+        </span>
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+      ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction/>
+      </div>
     </main>
-  )
+  );
 };
 
 export default PostPage;
